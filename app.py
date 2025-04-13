2cb91eb7d6a71639beab3634fe8a99ea472f9ef6
Arthur <rockymuriuki@gmail.com>
2025-04-14 00:18:23 +0300

    third commit

--- app.py
+++ app.py
@@ -1,12 +1,21 @@
-from flask import Flask
-import os
+from flask import Flask, render_template
 
 app = Flask(__name__)
 
 @app.route('/')
-def home():
-    return "Hello, world!"
+def Home():
+    return render_template('Home.html')  # Make sure home.html is in the 'templates' folder
+
+@app.route('/about')
+def aboutus():
+    return render_template('aboutus.html')  # You can add more routes to serve other pages
+@app.route('/give')
+def giving():
+    return render_template('giving.html')  # You can add more routes to serve other pages
+@app.route('/contact')
+def contactus():
+    return render_template('contactus.html')  # You can add more routes to serve other pages
+
 
 if __name__ == '__main__':
-    port = int(os.environ.get("PORT", 5000))  # 👈 render magic
-    app.run(host='0.0.0.0', port=port)
+    app.run(debug=True)
